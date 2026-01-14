edd70962c4b471827589cf9733b64218363fa44a
Faizan-Akram <100079631+faiz-ak@users.noreply.github.com>
2026-01-14 18:01:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
     try {
       const res = await axios.get(`${BACKEND_URL}/api/gifs`, {
         params: { q: getSearchKeyword(inputText) },
-        timeout: 70000,
+        timeout: 90000,
       });
       setGifs(res.data);
     } catch {
