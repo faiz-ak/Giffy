dcac224c1611d6dd827966c23f0498df8f78a817
faizan-blip <faizanrock753@gmail.com>
2026-01-20 00:15:50 +0530

    Fully landing + giffy + rainbow needs to edit

--- src/App.js
+++ src/App.js
@@ -9,47 +9,8 @@ import Giffy from "./components/Giffy";
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <>
       <Navbar />
-=======
-    <div className="container">
-  <h1>Giffy - A customized Gif Generator ✨</h1>
-{showWaitMessage && (
-   <p className="hint-text">
-  ⏳ Please wait <strong>up to 1 minute</strong> after clicking <strong>Generate GIF</strong>.
-  Our server may take a short time to wake up during the first run or after being idle for 15 minutes.
-</p>
-)}
- 
-<input
-  placeholder="User Friendly Prompt"
-  value={inputText}
-  onChange={(e) => setInputText(e.target.value)}
-/>
- 
-      <div className="btn-group">
-        <button onClick={generateGif}  disabled={loading}>
-          Generate GIF
-        </button>
- 
-        <button onClick={() => setShowText(!showText)}   disabled={loading}>
-          {showText ? "Remove Text" : "Add Text"}
-        </button>
-      </div>
- 
-      <div className="custom-panel">
-        <label>
-          Font Size [{fontSize}px]
-          <input
-            type="range"
-            min="18"
-            max="44"
-            value={fontSize}
-            onChange={(e) => setFontSize(+e.target.value)}
-          />
-        </label>
->>>>>>> 0d3def4d8426c95f2d6ba6bd48388849df9299ae
 
       <Routes>
         <Route path="/" element={<LandingLayout />} />
