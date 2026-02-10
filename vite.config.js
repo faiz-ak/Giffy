4390ace6bb8c8b273fdeffe2ac6318e013c86e32
Faizan-Akram <100079631+faiz-ak@users.noreply.github.com>
2026-02-10 14:29:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+   build: {
+    rollupOptions: {
+      external: ['mammoth']
+    }
+  },
   plugins: [react()],
   server: {
     port: 3000,
